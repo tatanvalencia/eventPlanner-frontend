8273c2e1d33e26f66a85af3fa33e7fe0188d43dd
Jhonatan Valencia Arango <jvalencia898@misena.edu.co>
2023-09-14 15:53:12 -0500

    initial commit

---
+++ src/app/events/events.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  exports: [
+    NavbarComponent
+  ],
+  declarations: [
+    NavbarComponent
+  ]
+})
+export class EventsModule { }
